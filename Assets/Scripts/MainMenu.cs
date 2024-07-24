ece76a9e1e8ac8144773012152e9176b797ab7ab
Karolart <karoljuzz1610@gmail.com>
2024-07-24 09:25:53 -0500

    main view complete

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Make sure this is included to work with UI elements
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenuUI;
     public GameObject settingsUI;
 
+    public Button Quit_button; // Ensure this is assigned in the Inspector
+
     void Start()
     {
         ShowMainMenu();
+
+        // Add listener to the Quit button
+        if (Quit_button != null)
+        {
+            Quit_button.onClick.AddListener(QuitGame);
+        }
+      
     }
 
     public void ShowMainMenu()
@@ -30,6 +40,12 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame()
     {
+        #if UNITY_EDITOR
+        // If running in the Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        // If running as a standalone application
         Application.Quit();
+        #endif
     }
 }
